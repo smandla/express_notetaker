559daf0b2eaf33cce6861279720745aa4229d5f7
smandla <35283559+smandla@users.noreply.github.com>
2022-07-20 13:49:34 -0700

    save note to json file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
 const noteData = require("./db/db.json");
 
-const PORT = 3001;
+const PORT = 3000;
 
 const app = express();
 
@@ -17,9 +18,23 @@ app.get("/", (req, res) => {
   //   console.log(__dirname);
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+app.get("/notes", (req, res) => {
+  console.log("in here");
+  //   console.log(__dirname);
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.get("/api/notes", (req, res) => res.json(noteData));
+app.post("/api/notes", (req, res) => {
+  console.log(req.body);
+  noteData.push(req.body);
+  console.log(noteData);
+  //   console.log(req.query);
 
-app.get("/api/pets", (req, res) => res.json(noteData));
-
+  fs.writeFile("./db/db.json", JSON.stringify(noteData), (err) =>
+    err ? console.error(err) : console.log("Success")
+  );
+  res.send(noteData);
+});
 app.listen(PORT, () => {
   console.log(`Example app listening at https://localhost:${PORT}`);
 });
