66c136075f325b18b3c77dc895d449c78700d1f8
smandla <35283559+smandla@users.noreply.github.com>
2022-07-24 12:10:12 -0700

    fix PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const noteData = require("./db/db.json");
 const { random } = require("colors");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
